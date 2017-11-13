644e2446d4e5656918633dbb133ea8c897433cde
Cphil31 <cecilephilippe31@gmail.com>
2017-11-13 16:32:53 +0100

    mise en page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,56 +10,72 @@
 
 	<div class="ui grid">
 		<div class="six wide column"></div>
-		<div class="eight wide column">
+		<div class="four wide column"><h1>Calculatrice</h1></div>
+	</div>
 
-			<h1>Calculatrice</h1>
+	<div class="ui grid">
+		<div class="five wide column"></div>
+		<div class="four wide column">
 
-			
-			<div class="ui input">
+			<div class="ui fluid input">
 				<input type="text" placeholder="">
 			</div>
+			
+		</div>
+	</div>
 
 
-			<div class="ui grid">
-				<div class="seven wide column">
-					
-					<button class="ui button">1</button>
+	<div class="ui grid">
+		<div class="five wide column"></div>
+		<div class="three wide column">
 
-					<button class="ui button">2</button>
+			<button class="ui button">1</button>
 
-					<button class="ui button">3</button>
+			<button class="ui button">2</button>
 
-					<button class="ui button">+</button>
+			<button class="ui button">3</button>
 
-					<button class="ui button">4</button>
 
-					<button class="ui button">5</button>
+			<button class="ui button">4</button>
 
-					<button class="ui button">6</button>
+			<button class="ui button">5</button>
 
-					<button class="ui button">-</button>
+			<button class="ui button">6</button>
 
-					<button class="ui button">7</button>
 
-					<button class="ui button">8</button>
+			<button class="ui button">7</button>
 
-					<button class="ui button">9</button>
+			<button class="ui button">8</button>
 
-					<button class="ui button">*</button>
+			<button class="ui button">9</button>
 
-					<button class="ui button">0</button>
 
-					<button class="ui button">/</button>
-
-					<button class="ui button">=</button>
-					
-				</div>
-
-			</div>
+			<button class="ui button">0</button>
 
 		</div>
-		
+		<div class="one wide column">
+			<div class="plus">
+				<button class="ui button">+</button>
+				<button class="ui button">-</button>
+				<button class="ui button">*</button>
+				<button class="ui button">/</button>
+			</div>
+			
+		</div>
+
 	</div>
+
+
+	<div class="ui grid">
+		<div class="five wide column"></div>
+		<div class="three wide column">
+
+			<button class="ui fluid button">=</button>
+			
+		</div>
+	</div>
+
+
 
 
 	<script src="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.js"></script>
