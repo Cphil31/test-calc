b34dc0e5b2c2e270da906b2424b66de414d88dde
Cphil31 <cecilephilippe31@gmail.com>
2017-11-13 16:04:04 +0100

    ajout bouttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,36 +7,60 @@
 	<link rel="stylesheet" href="../css/app.css">
 </head>
 <body>
-	<h1>Calculatrice</h1>
+
+	<div class="ui grid">
+		<div class="six wide column"></div>
+		<div class="eight wide column">
+
+			<h1>Calculatrice</h1>
+
+			
+			<div class="ui input">
+				<input type="text" placeholder="">
+			</div>
 
 
-	<button class="ui button">1</button>
+			<div class="ui grid">
+				<div class="seven wide column">
+					
+					<button class="ui button">1</button>
 
-	<button class="ui button">2</button>
+					<button class="ui button">2</button>
 
-	<button class="ui button">3</button>
+					<button class="ui button">3</button>
 
-	<button class="ui button">4</button>
+					<button class="ui button">+</button>
 
-	<button class="ui button">5</button>
+					<button class="ui button">4</button>
 
-	<button class="ui button">6</button>
+					<button class="ui button">5</button>
 
-	<button class="ui button">7</button>
+					<button class="ui button">6</button>
 
-	<button class="ui button">8</button>
+					<button class="ui button">-</button>
 
-	<button class="ui button">9</button>
+					<button class="ui button">7</button>
 
-	<button class="ui button">0</button>
-	
-	<button class="ui button">+</button>
+					<button class="ui button">8</button>
 
-	<button class="ui button">-</button>
+					<button class="ui button">9</button>
 
-	<button class="ui button">*</button>
+					<button class="ui button">*</button>
 
-	<button class="ui button">/</button>
+					<button class="ui button">0</button>
+
+					<button class="ui button">/</button>
+
+					<button class="ui button">=</button>
+					
+				</div>
+
+			</div>
+
+		</div>
+		
+	</div>
+
 
 	<script src="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.js"></script>
 	<script src="../js/app.js"></script>
